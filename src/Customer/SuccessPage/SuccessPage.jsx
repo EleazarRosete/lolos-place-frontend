8f422ad47094a44545be823dae7124d7522ee7dc
meh <103293269+mekelcruzz@users.noreply.github.com>
2025-01-06 23:33:22 +0800

    Update SuccessPage.jsx

--- src/Customer/SuccessPage/SuccessPage.jsx
+++ src/Customer/SuccessPage/SuccessPage.jsx
@@ -254,26 +254,7 @@ const SuccessPage = () => {
   
 
   useEffect(() => {
-    const fetchPaymentStatus = async () => {
-      const user_id = customer.id;
-      try {
-        const response = await axios.get(`https://lolos-place-backend.onrender.com/api/check-payment-status/${user_id}`);
-        if (sessionId === response.data.session_id && response.data.payment_status === 'pending') {
-          if (isAdvanceOrder) {
-            handleReservation();
-          } else {
-            handleConfirmOrder();
-          }
-        } else {
-          navigate('/');
-        }
-      } catch (error) {
-        console.error('Error checking payment status:', error.message);
-        navigate('/');
-      }
-    };
-
-    fetchPaymentStatus();
+    handleConfirmOrder();
   }, []);
 
   return (
